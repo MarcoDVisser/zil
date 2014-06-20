d9745e41d111f2f227611a3b919d00494f9686b4
Marco Visser <marco.d.visser@gmail.com>
2014-06-20 15:37:53 +0200

    init commit

---
+++ R/zil.R
@@ -0,0 +1,89 @@
+##' Density, distribution function, quantile function and random
+##' generation for the Zero Inflated Laplace distribution with mean equal
+##' to 'mu', shape 'b' and zero inflation 'p'. 
+##'
+##' The mean 'mu' sets the central tendency of the Laplace, with "b"
+##' defining the spread around 'mu'. The zero inflation parameter
+##' "p", defines the point mass density above 0. The density under
+##' zero relates to p as (1/(1+1)) while the probabilty density not
+##' equal to zero is given by 1-(1/(1+1)). The function "rzil" is based
+##' on the inverse cummulative function, and translates uniform
+##' random numbers to their zil equivalents. 
+##' 
+##' @param mu the mean of the non-zero density
+##' @param b the shape parameter defining spread around mu
+##' (must be >0).
+##' @param p the zero inflation parameter (must be >0) 
+##' @author Marco D. Visser
+##' @rdname dzil
+##' @examples
+##' curve(dnorm(x,mu=5),-2,10)
+##' x<-rzil(100,mu=5)
+##' hist(x)
+##' @title The Zero Inflated Laplace 
+##' @seealso \code{\link{dnorm}}, \code{\link{dunif}},
+##' or \code{\link{runif}}.
+##' @concept probabilty density
+##' @export
+dzil <- function(x,mu=0,b=1,p=1){
+
+  if(sum(p<0)+sum(b<0)){stop("b or p < 0")}
+
+  {(p*0^abs(x))+{exp(-abs(x-mu)/b)/(2*b)}}/(1+p)
+ 
+}
+
+##' Density, distribution function, quantile function and random
+##' generation for the Zero Inflated Laplace distribution with mean equal
+##' to 'mu', shape 'b' and zero inflation 'p'. 
+##'
+##' @rdname dzil
+##' @export
+pzil <- function(x,mu=0,b=1,p=1){
+
+  if(sum(p<0)+sum(b<0)){stop("b or p < 0")}
+  
+  plp <- ifelse(x<mu,.5*exp((x-mu)/b),
+                1-(0.5*exp(-((x-mu)/b)))) *(1/(p+1))
+
+  ifelse(x>=0,plp+(1-(p+1)^(-1)),plp)
+}
+
+##' Density, distribution function, quantile function and random
+##' generation for the Zero Inflated Laplace distribution with mean equal
+##' to 'mu', shape 'b' and zero inflation 'p'. 
+##'
+##' @rdname dzil
+##' @export
+qzil <- function(q,mu=0,b=1,p=1){
+  
+  if(sum(p<0)+sum(b<0)){stop("b or p < 0")}
+  if(sum(q<0|q>1)){stop("q not a valid quantile")}
+
+  qden <- c(pzil(0-.Machine$double.eps,mu=mu,b=b,p=p),
+            pzil(0,mu=mu,b=b,p=p))
+  
+  x <- ifelse(q>qden[1]&q<qden[2],0,
+                   ifelse(q<=qden[1],
+                          qlp(q/(1/(1+1)),mu=mu,b=b),
+                          qlp((q-diff(qden))/(1/(1+1)),mu=mu,
+                              b=b)))
+  return(x)
+}
+
+##' Density, distribution function, quantile function and random
+##' generation for the Zero Inflated Laplace distribution with mean equal
+##' to 'mu', shape 'b' and zero inflation 'p'. 
+##'
+##' @rdname dzil
+##' @export
+rzil <- function(n,mu=0,b=1,p=1){
+
+  qzil(runif(n,0+.Machine$double.eps,
+             1-.Machine$double.eps),mu=mu,b=b,p=p)
+  
+}
+
+##' Helper function in qzil
+##' @rdname dzil
+qlp <-function(q,mu,b) mu-b*sign(q-0.5)*log(1-2*abs(q-0.5))
--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-zil
+`zil` The Zero Inflated Laplace Distribution
 ===
 
-The Zero Inflated Laplace Distribution
+The `zil` package extends R with the Zero Inflated Laplace Distribution and provides functions for calculation of the density, distribution and quantile functions including a random number generation. The Zero Inflated Laplace is a flexible distribution used for e.g. meassurment error modeling where where a fraction (1/(1+p)) of errors can be zero, while also allowing for potential bias (parameter 'mu') and large spread in errors (parameter 'b').
+
+
+## Installation
+
+Currently there isn't a release on [CRAN](http://cran.r-project.org/),
+though there may one day be one. You can still  
+download the [zip](https://github.com/MarcoDVisser/zil/zipball/master) 
+or [tar ball](https://github.com/MarcoDVisser/zil/tarball/master).
+Then decompress and run `R CMD INSTALL` on it, 
+or use the **devtools** package to install the development version.
+
+```r
+## Make sure your current packages are up to date
+update.packages()
+## devtools is required
+library(devtools)
+install_github("zil", "MarcoDVisser")
+```
+## Examples
+
+```r
+# using the density function and random numbers
+x<-rzil(1000,mu=5)
+hist(x,freq=FALSE)
+lines(-5:15,dzil(-5:15,mu=5),lwd=3,col='green')
+```
+![](http://i.imgur.com/QxeArJm.png)
+
+Here parameters include, the  mean 'mu' sets the central tendency of the Laplace, with "b"  defining the spread around 'mu'. The zero inflation parameter  "p", defines the point mass density above 0. The density under  zero relates to p as (1/(1+1)) while the probabilty density not  equal to zero is given by 1-(1/(1+1)). The function "rzil" is based  on the inverse cummulative function, and translates uniform random numbers to their zil equivalents. 
+
